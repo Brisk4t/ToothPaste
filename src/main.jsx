d33030e344e5fcebd6b58d41ddb74b7c80aee701
Brisk4t <atharvapcat@gmail.com>
2025-12-26 12:30:07 -0700

    Add error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ErrorBoundary } from "react-error-boundary";
 import { ThemeProvider } from '@material-tailwind/react';
 import { theme } from './styles/theme';
 
+function ErrorFallback({ error }) {
+  return <div>Oops: {error.message}</div>;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider value={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <ErrorBoundary FallbackComponent={ErrorFallback}onError={(error) => console.error(error)}>
+        <ThemeProvider value={theme}>
+            <App />
+          </ThemeProvider>
+        </ErrorBoundary>
+      </React.StrictMode>
 );
